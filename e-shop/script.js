3103a05c57709b46d943198092a2879db8145469
alexandraSS <alexa434343@gmail.com>
2021-10-10 14:16:50 +0300

    дз по уроку 3

--- e-shop/script.js
+++ e-shop/script.js
@@ -1,16 +1,33 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const GOODS_LIST = '/catalogData.json';
+const CARD_LIST = '/getBasket.json';
+const ADD_GOOD_TO_BASKET = '/addToBasket.json';
+const REMOVE_GOOD_FROM_CARD = '/deleteFromBasket.json';
 
+const serverRequest = function (method, postfix) {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, `${API}${postfix}`, true);
+    xhr.send();
+    xhr.onload = (e) => {
+      resolve(JSON.parse(e.target.responseText));
+    };
+  })
+}
 
 class GoodsItem {
-  constructor(title = 'default title', price = 0) {
-    this.title = title;
+  constructor(product_name = 'default title', price = 0, id_product = 123) {
+    this.product_name = product_name;
     this.price = price;
+    this.id = id_product;
   }
   render() {
     return `
         <div class="goods__item">
           <div class="goods__img"></div>
-          <h3 class="goods__title">${this.title}</h3>
+          <h3 class="goods__title">${this.product_name}</h3>
           <div class="goods__price">${this.price}</div>
+          <button class="btnAdd" data-id='${this.id}'>Добавить</button>
         </div>
       `
   }
@@ -20,71 +37,91 @@ class GoodsList {
   goods = [];
   constructor() {
     this.fetchGoods();
-    this.render();
   }
+
   fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 }
-    ];
+    serverRequest('GET', GOODS_LIST).then((goods) => {
+      this.goods = goods;
+      this.render();
+    });
   }
-  sumPrices() {
-    let totalPrice = 0;
 
-    for (let i = 0; i < this.goods.length; i++) {
-      totalPrice += this.goods[i].price;
-    }
-
-    return totalPrice;
-  }
   render() {
-    const goodsItems = this.goods.map(({ title, price }) => {
-      const goodsItem = new GoodsItem(title, price);
+    const goodsItems = this.goods.map(({ product_name, price }) => {
+      const goodsItem = new GoodsItem(product_name, price);
       return goodsItem.render();
     });
     document.querySelector('.goods').innerHTML = goodsItems.join('');
   }
 }
 
-class Product {
-  constructor(title, price) {
-    this.title = title;
+class CardItem {
+  constructor(id_product, product_name, price, quantity) {
+    this.id_product = id_product;
+    this.product_name = product_name;
     this.price = price;
+    this.quantity = quantity;
+  }
+  render() {
+    return `
+        <div class="goods__item goods__item_cart">
+          <div>
+            <h3 class="goods__title">${this.product_name}</h3>
+            <div class="goods__price">${this.price}</div>
+          </div>
+          <button>Удалить</button>  
+        </div>
+      `
   }
 }
 
 class Cart {
   goodsInCart = [];
+  amount = 0;
+  count = 0;
 
   constructor() {
-
+    this.fetchCartContents();
   }
 
-  addProduct() {
+  fetchCartContents() {
+    serverRequest('GET', CARD_LIST).then((goods) => {
+      this.amount = goods.amount;
+      this.count = goods.countGoods;
+      this.goodsInCart = goods.contents;
 
-  }
-
-  removeProduct() {
-
-  }
-
-  changeAmount() {
-
-  }
-
-  sum() {
-
+      this.render();
+    });
   }
 
   render() {
-
+    const goodsItemsInCart = this.goodsInCart.map(({ id_product, product_name, price, quantity }) => {
+      const goodsItemInCart = new CardItem(id_product, product_name, price, quantity);
+      return goodsItemInCart.render();
+    });
+    document.querySelector('.cart').innerHTML = `
+      <div> ${goodsItemsInCart.join('')} </div>
+      <div>Общее количество товаров: ${this.count}</div>
+      <div>Итого сумма товаров: ${this.amount}</div>
+    `;
   }
+
+  // removeProduct() { }
+
+  // addProduct() { }
+
+  // changeAmount() { }
+
+  // sum() { }
 }
 
 onload = () => {
   const goodsList = new GoodsList;
 
-  console.log(goodsList.sumPrices());
+  const cart = new Cart;
+
+  document.getElementById('cartBtn').onclick = function () {
+    let cart = document.getElementById('cart');
+    cart.classList.toggle("cart_visible");
+  }
 }
